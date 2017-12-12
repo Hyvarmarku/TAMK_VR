ab865f4ba517d0a76f98a8200ce542c6aa289253
DESKTOP-0O04LD4\Markus <markus.hyvarinen@cs.tamk.fi>
2017-12-12 15:19:54 +0200

    Material fix

---
+++ Assets/Scripts/Test/CameraMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField] private float _movementSpeed = 0.05f;
+    [SerializeField] private float _turningSpeed = 0.05f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * _movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * _movementSpeed;
+        }
+        if(Input.GetKey(KeyCode.R))
+        {
+            var newPosition = transform.position;
+            newPosition.y += 0.05f;
+            transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            var newPosition = transform.position;
+            newPosition.y -= 0.05f;
+            transform.position = newPosition;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            var newRotation = transform.eulerAngles;
+            newRotation.y -= _turningSpeed;
+            transform.eulerAngles = newRotation;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var newRotation = transform.eulerAngles;
+            newRotation.y += _turningSpeed;
+            transform.eulerAngles = newRotation;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            var newRotation = transform.eulerAngles;
+            newRotation.x -= _turningSpeed;
+            transform.eulerAngles = newRotation;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            var newRotation = transform.eulerAngles;
+            newRotation.x += _turningSpeed;
+            transform.eulerAngles = newRotation;
+        }
+
+    }
+}
